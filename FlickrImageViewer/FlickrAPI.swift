080e05ce3c4d7e771819658de5c3e727cb9188ec
sojideveloper <iwritecode@gudugba.local>
2017-04-17 01:49:21 -0500

    Add UI
    Add imageView and button

---
+++ FlickrImageViewer/FlickrAPI.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrAPI.swift
+//  FlickrImageViewer
+//
+//  Created by iwritecode on 4/17/17.
+//  Copyright © 2017 iwritecode. All rights reserved.
+//
+
+import Foundation
+
+struct FlickrAPI {
+    
+    struct URLArguments {
+        
+    }
+    
+}
---
+++ FlickrImageViewer/ImageViewerViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ImageViewerViewController.swift
+//  FlickrImageViewer
+//
+//  Created by iwritecode on 4/17/17.
+//  Copyright © 2017 iwritecode. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - ImageViewerViewController
+
+class ImageViewerViewController: UIViewController {
+    
+    // MARK: View lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
