9ca5f1785b997f3a5359060f57428e2002070fd6
sojideveloper <iwritecode@gudugba.local>
2017-04-17 02:58:33 -0500

    Add code
    Add API function

--- FlickrImageViewer/FlickrAPI.swift
+++ FlickrImageViewer/FlickrAPI.swift
@@ -8,9 +8,48 @@
 
 import Foundation
 
+// MARK: - FlickrAPI
+
 struct FlickrAPI {
     
-    struct URLArguments {
+    enum Method: String {
+        case
+            RecentPhotos = "flickr.photos.getRecent",
+            GetPhotos = "flickr.galleries.getPhotos"
+    }
+    
+    private static let baseURLString = "https://api.flickr.com/services/rest"
+    private static let APIKey = ""
+    
+    
+    
+    private static func flickrURL(method: Method, parameters: [String : String]?) -> URL? {
+        
+        var components = URLComponents(string: baseURLString)
+        var queryItems = [URLQueryItem]()
+        
+        let baseParameters = [
+            "api_key" : APIKey,
+            "method" : method.rawValue,
+            "format" : "json",
+            "nojsoncallback" : "1",
+        ]
+        
+        for (key, value) in baseParameters {
+            let item = URLQueryItem(name: key, value: value)
+            queryItems.append(item)
+        }
+        
+        if let additionalParameters = parameters {
+            for (key, value) in additionalParameters {
+                let item = URLQueryItem(name: key, value: value)
+                queryItems.append(item)
+            }
+        }
+        
+        components?.queryItems = queryItems
+        
+        return components?.url!
         
     }
     
